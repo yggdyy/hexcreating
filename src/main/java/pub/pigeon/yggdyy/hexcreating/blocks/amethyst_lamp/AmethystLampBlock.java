4194f4a539a6c0adfa43667d8ebe358cff49d05e
yggdyy <chengpeng217@163.com>
2025-05-25 09:50:31 +0800

    amethyst_lamp is now wrenchable and craftable

--- src/main/java/pub/pigeon/yggdyy/hexcreating/blocks/amethyst_lamp/AmethystLampBlock.java
+++ src/main/java/pub/pigeon/yggdyy/hexcreating/blocks/amethyst_lamp/AmethystLampBlock.java
@@ -1,16 +1,19 @@
 package pub.pigeon.yggdyy.hexcreating.blocks.amethyst_lamp;
 
+import com.simibubi.create.content.equipment.wrench.IWrenchable;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
 import org.jetbrains.annotations.Nullable;
 
-public class AmethystLampBlock extends Block {
+public class AmethystLampBlock extends Block implements IWrenchable {
     public AmethystLampBlock(Settings settings) {
         super(settings.luminance(state -> state.get(Properties.POWER)));
     }
@@ -35,5 +38,11 @@ public class AmethystLampBlock extends Block {
     @Override
     public BlockState getPlacementState(ItemPlacementContext itemPlacementContext) {
         return super.getPlacementState(itemPlacementContext).with(Properties.POWER, 0);
+    }
+    @Override
+    public ActionResult onWrenched(BlockState state, ItemUsageContext context) {
+        context.getWorld().setBlockState(context.getBlockPos(), state.cycle(Properties.POWER), 3);
+        playRotateSound(context.getWorld(), context.getBlockPos());
+        return ActionResult.SUCCESS;
     }
 }
